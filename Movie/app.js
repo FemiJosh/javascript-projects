a7da84b2761890d42b6ba83243bf4fa4c6d4efd9
FemiJosh <femijoshua679@GMAIL.COM>
2023-02-20 04:48:52 +0100

    Add files via upload

--- Movie/app.js
+++ Movie/app.js
@@ -6,12 +6,158 @@ const logoCont = document.querySelector('.logo-container');
 const loginPage = document.querySelector('.login-page');
 const registerPage = document.querySelector('.register-page');
 const loginCont = document.querySelectorAll('.login-and-register');
-const movieContEl = document.querySelector('.movie-box-container');
+const movieContEl = document.querySelectorAll('.movie-box-container');
 const menuBtns = document.querySelector(".menu-btns");
 const remainderCont = document.querySelector('.remainder-container');
 const body = document.querySelector('body');
+const moreBtn = document.querySelector('.morebtn');
+
 
                 //SELECTING ELEMENTS ENDS
+
+
+                // GETTING APIS AND DISPLAYING
+
+                //OPEING MOVIES STARTS 
+    let eachMovies = '';
+
+ function getPicture(posterpath){
+                return `https://www.themoviedb.org/t/p/original${posterpath}`
+            }
+
+            let num = 1;
+    // let movieUrl = ``
+
+            function displayMovie(num){
+                fetch(`https://api.themoviedb.org/3/movie/popular?api_key=d1715382eeb174fd056f98ce782a6135&language=en-US&page=${num}`).then(response => response.json())
+                .then(res => {
+                    let allData = res.results;
+                    eachMovies = allData.map(eachMovie =>{
+                        return `<div class="theboxes">
+                        <img src= ${getPicture(eachMovie.poster_path)} class="box-img">
+                        <p class="movie-title"> ${eachMovie.title}</p>
+                        <h3 class="movie-category"> ${eachMovie.release_date} </h3>
+                            <button class="btns remainder-btn">
+                                set remainder
+                            </button>
+                        </div>
+                    </div>`
+                    }).join('')
+                    movieContEl[0].innerHTML += eachMovies;
+                    displayRemainderBtn();
+
+                })
+                .catch( err => new Error )
+            }
+                //OPEING MOVIES ENDS 
+
+
+
+                // DISPLAY COMING SOON MOVIES STARTS 
+function displayComingMovies(){
+    fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=d1715382eeb174fd056f98ce782a6135&language=en-US&page=1`)
+    .then(res => res.json())
+    .then (results => {
+        let realData = results.results
+        eachMovies = realData.map(eachMovie =>{
+            return `<div class="theboxes">
+            <img src= ${getPicture(eachMovie.poster_path)} class="box-img">
+            <p class="movie-title"> ${eachMovie.title}</p>
+            <h3 class="movie-category"> ${eachMovie.release_date} </h3>
+        </div>`
+        }).join('')
+        movieContEl[1].innerHTML += eachMovies;       
+    })
+    .catch( err => {
+        new Error
+    console.log(err) })
+        }
+
+                // DISPLAY COMING SOON MOVIES ENDS 
+
+
+                // DISPLAY NEWS AND OTHERS STARTS
+const newsBoxContEl = document.querySelector('.newsbox-container')
+                const options = {
+                    method: 'GET',
+                    headers: {
+                        'X-RapidAPI-Key': '6190eee07fmsh775b0400439c646p129cf0jsnb377dcba7b32',
+                        'X-RapidAPI-Host': 'crypto-news16.p.rapidapi.com'
+                    }
+                };
+                
+                fetch('https://crypto-news16.p.rapidapi.com/news/top/10', options)
+                    .then(response => response.json())
+                    .then(response => {
+                       let eachNews =  response.map(news =>{
+                            return ` <ul class="news-list">
+                            <li> <a href="${news.url}" class="news"> ${news.title}</a></li>
+                            <span class = 'date'> ${news.date}</span>
+                            </ul>
+                            `  
+                        }).join('');
+                        newsBoxContEl.innerHTML = eachNews;
+                    })
+
+
+                    .catch(err => console.error(err));
+
+
+                // DISPLAY NEWS AND OTHERS  ENDS 
+
+
+                            // GETTING APIS AND DISPLAYING ENDS 
+
+
+                //GETTING THE GENRES
+
+                // function fetchGenres(){
+                //     fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=d1715382eeb174fd056f98ce782a6135&language=en-US`)
+                //     .then(res => res.json())
+                //     .then (results => {
+                //         let realData = results.genres
+                        
+                   
+
+                        // eachMovies = realData.map(eachMovie =>{
+                        //     return `<div class="theboxes">
+                        //     <img src= ${getPicture(eachMovie.poster_path)} class="box-img">
+                        //     <p class="movie-title"> ${eachMovie.title}</p>
+                        //     <h3 class="movie-category"> ${eachMovie.release_date} </h3>
+                        // </div>`
+                        // }).join('')
+                        // movieContEl[1].innerHTML += eachMovies; 
+                        
+                        
+
+                             // let genresArray = [];
+                        // genresArray[0] = { "id":`${realData.id}` , "name": `${realData.name}`}
+                        // genresArray[1] = { "id":`${realData.id}` , "name": `${realData.name}`}
+
+                        // let tempData = [];
+                        // for (let index = 0 ; index<genresArray.length ;index++){
+                        //     if(genresArray[index].name == `${realData.name}`){
+                        //         tempData.push(genresArray)
+                        //     }
+                        // }
+                        // genresArray =tempData;
+
+                        // console.log(tempData)
+                        
+                    // })
+                    // .catch( err => {
+                    //     new Error
+                    // console.log(err) })
+                    //     }
+
+                    //     fetchGenres();
+
+
+                //ENDING THE GENRES
+
+
+
+
 
 
                 // DISPLAY MOIVES AND CATEGORIES
@@ -19,88 +165,85 @@ let remainderBtn;
 let isAlarmSet = false;
 
 
-const moviesList = [
-    {
-    id: "1",
-    img: "./Images/PopCorn.jpg",
-    movieTitle: 'Venom',
-    movieCategory: 'Action',
-    movieTime: '100',
-    },
-    {
-        id: "2",
-        img: "./Images/PopCorn.jpg",
-        movieTitle: 'SuperStory',
-        movieCategory: 'Drama',
-        movieTime: '150',
-    },
-    {
-        id: "3",
-        img: "./Images/PopCorn.jpg",
-        movieTitle: 'Willi Willi',
-        movieCategory: 'Tragedy',
-        movieTime: '120',
-    },
-    {
-        id: "4",
-        img: "./Images/PopCorn.jpg",
-        movieTitle: 'Baba Suwe',
-        movieCategory: 'Comedy',
-        movieTime: '110',
-        },
-        {
-            id: "5",
-            img: "./Images/PopCorn.jpg",
-            movieTitle: 'Ayanmatangan',
-            movieCategory: 'Thriller',
-            movieTime: '170',
-            },
+                //DISPLAY SEARCHED MOVIES START
+                fetch('https://imdb8.p.rapidapi.com/auto-complete?q=friend')
+                    .then(response => response.json())
+                    .then(response => console.log(response))
+                    .catch(err => console.error(err));
+                //DISPLAY SEARCHED MOVIES ENDS
 
-            {
-                id: "5",
-                img: "./Images/PopCorn.jpg",
-                movieTitle: 'Ayanmatangan',
-                movieCategory: 'Thriller',
-                movieTime: '170',
-                },
-                {
-                    id: "5",
-                    img: "./Images/PopCorn.jpg",
-                    movieTitle: 'Ayanmatangan',
-                    movieCategory: 'Thriller',
-                    movieTime: '170',
-                    }, {
-                        id: "2",
-                        img: "./Images/PopCorn.jpg",
-                        movieTitle: 'SuperStory',
-                        movieCategory: 'Drama',
-                        movieTime: '150',
-                    },
 
-        ]
-    let eachMovies = '';
+// const moviesList = [
+//     {
+//     id: "1",
+//     img: "./Images/PopCorn.jpg",
+//     movieTitle: 'Venom and every other reubbis that wants to be causing me wahala on this code',
+//     movieCategory: 'Action',
+//     movieTime: '100',
+//     },
+//     {
+//         id: "2",
+//         img: "./Images/PopCorn.jpg",
+//         movieTitle: 'SuperStory',
+//         movieCategory: 'Drama',
+//         movieTime: '150',
+//     },
+//     {
+//         id: "3",
+//         img: "./Images/PopCorn.jpg",
+//         movieTitle: 'Willi Willi',
+//         movieCategory: 'Tragedy',
+//         movieTime: '120',
+//     },
+//     {
+//         id: "4",
+//         img: "./Images/PopCorn.jpg",
+//         movieTitle: 'Baba Suwe',
+//         movieCategory: 'Comedy',
+//         movieTime: '110',
+//         },
+//         {
+//             id: "5",
+//             img: "./Images/PopCorn.jpg",
+//             movieTitle: 'Ayanmatangan',
+//             movieCategory: 'Thriller',
+//             movieTime: '170',
+//             },
 
-function displayMovie(list){
-    eachMovies = list.map(eachMovie =>{
-        return `<div class="theboxes">
-        <img src=${eachMovie.img} class="box-img">
-        <p class="movie-title"> ${eachMovie.movieTitle}</p>
-        <h3 class="movie-category"> ${eachMovie.movieCategory} </h3>
-        <span class="split"> | </span>
-        <span class="movie-time"> ${eachMovie.movieTime} min</span>
-            <button class="btns remainder-btn">
-                set remainder
-            </button>
-        </div>
-    </div>`
-    }).join('')
-    movieContEl.innerHTML = eachMovies;
-    displayRemainderBtn();
-}
+//             {
+//                 id: "5",
+//                 img: "./Images/PopCorn.jpg",
+//                 movieTitle: 'Ayanmatangan',
+//                 movieCategory: 'Thriller',
+//                 movieTime: '170',
+//                 },
+//                 {
+//                     id: "5",
+//                     img: "./Images/PopCorn.jpg",
+//                     movieTitle: 'Ayanmatangan',
+//                     movieCategory: 'Thriller',
+//                     movieTime: '170',
+//                     }, {
+//                         id: "2",
+//                         img: "./Images/PopCorn.jpg",
+//                         movieTitle: 'SuperStory',
+//                         movieCategory: 'Drama',
+//                         movieTime: '150',
+//                     },
+
+//         ]
+
+
 window.addEventListener('load', ()=>{
-    displayMovie(moviesList);
+    displayMovie(1);
+    displayComingMovies()
 })
 
+moreBtn.addEventListener('click',()=>{
+    num++;
+    displayMovie(num)
+    console.log(num)
+})
 
 let reducedMovies = moviesList.reduce((presentResult , presentItem)=>{
    
@@ -148,7 +291,7 @@ categoryBtns.forEach(catBtn =>{
 })
 
 function displayRemainderBtn(){
-    let eachBox = movieContEl.querySelectorAll('.theboxes')
+    let eachBox = movieContEl[0].querySelectorAll('.theboxes')
     eachBox.forEach(box =>{
         let remainderBtn = box.querySelector('.remainder-btn');
         box.addEventListener('click' , ()=>{
@@ -156,12 +299,17 @@ function displayRemainderBtn(){
                 let remainderBtn = item.querySelector('.remainder-btn');
                 if(item !== box){
                     remainderBtn.classList.remove('display-remainder-btn');
+                    item.classList.remove('for-image-box')
                 }
             })
             remainderBtn.classList.toggle('display-remainder-btn');
+            box.classList.toggle('for-image-box');
+
 
             remainderBtn.addEventListener('click',()=>{
                 pageDisplay(remainderCont);
+
+                console.log(eachBox)
                 // if(isAlarmSet){
                 //     remainderBtn.innerHTML = 'Set Remainder';
                 //     alarmTime = ''
@@ -302,16 +450,45 @@ function removePage(element){
         bokkedMoviesCountEl.innerText = count;
 
 
-        // if(alarmTime.includes('Hour')||alarmTime.includes('Minutes')||alarmTime.includes('AM/PM')){
-        //     return alert('Please Input a Correct Time');
-        // }        
+        if(alarmTime.includes('Hour')||alarmTime.includes('Minutes')||alarmTime.includes('AM/PM')){
+            return alert('Please Input a Correct Time');
+        }        
         removePage(remainderCont)
     })
     
                //REMAINDER FUNCTIONS AND SETTINGS ENDS 
 
 
-            //MOVIE DATABASE 
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    //MOVIE DATABASE 
 
 //     let mainData = fetch('https://api.simkl.com/movies/trending/interval?client_id=f02fa03d35e73db269af2f7784770ec5f266ce492fe8f99a41023c0caaa48b70')
 //     .then( data => data.json())
@@ -343,15 +520,10 @@ function removePage(element){
 // 	.then(response => console.log(response))
 // 	.catch(err => console.error(err));
 
-    const options = {
-        method: 'POST',
-        headers: {
-            'X-RapidAPI-Key': '6190eee07fmsh775b0400439c646p129cf0jsnb377dcba7b32',
-            'X-RapidAPI-Host': 'iwatchonline.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://iwatchonline.p.rapidapi.com/http://www.iwatchonline.to/api/addlinks', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+    // const options = {
+    //     method: 'POST',
+    //     headers: {
+    //         'X-RapidAPI-Key': '6190eee07fmsh775b0400439c646p129cf0jsnb377dcba7b32',
+    //         'X-RapidAPI-Host': 'iwatchonline.p.rapidapi.com'
+    //     }
+    // };
